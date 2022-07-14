5f5b52a974b0383f1de988efa396ecbd50c687cc
lim6112j <lim6112j@gmail.com>
2022-07-14 13:19:05 +0900

    bug

--- scripts/seed_data.sql
+++ scripts/seed_data.sql
@@ -5,7 +5,7 @@ VALUES
 ('오렌지', 1, 'Brazil', 'pretty yellow gecko','orange', 1,2,'assets/images/orange.jpeg', 'img1, img2', '1/2/4/'),
 ('레모니', 2, 'America', 'pretty pink gecko','lemon',1,2,'assets/images/lemon_frost_resized.png', 'img1, img2', '1/2/3/'),
 ('모니', 2, 'USA', 'pretty violet gecko','lemon',1,2,'assets/images/lemon_frost_resized.png', 'img1, img2', '1/2/3/4/6/7/8/'),
-('utey', 2, 'Canada', 'pretty skyblue gecko','yellow',1,2,'assets/images/yellow.jpeg', 'img1, img2', '2/');
+('utey', 2, 'Canada', 'pretty skyblue gecko','yellow',1,2,'assets/images/yellow.jpeg', 'img1, img2', '2/'),
 ('랑이', 4, 'Indonesia', 'pretty skyblue gecko','blue',1,2,'assets/images/blue.png', 'img1, img2', '1/3/');
 
 INSERT INTO `geckodb`.`users` (`name`, `age`, `desc` )
