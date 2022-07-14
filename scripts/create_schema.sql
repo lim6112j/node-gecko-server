f36781df12995db5c9b674acf2ea7ed0ceb97567
lim6112j <lim6112j@gmail.com>
2022-07-14 13:10:33 +0900

    sql scripts modified

--- scripts/create_schema.sql
+++ scripts/create_schema.sql
@@ -15,9 +15,13 @@ CREATE TABLE `geckodb`.`geckos` (
   `name` VARCHAR(45) NOT NULL,
   `age` INT NULL,
   `origin` VARCHAR(255) NULL,
+  `color` VARCHAR(45) NULL,
   `desc` TEXT NULL,
   `father` INT NULL,
   `mother` INT NULL,
+  `thumbnail` VARCHAR(255) NOT NULL,
+  `images` VARCHAR(255),
+  `ancestry` VARCHAR(100),
   PRIMARY KEY (`id`));
 
 CREATE TABLE `geckodb`.`users` (
--- scripts/seed_data.sql
+++ scripts/seed_data.sql
@@ -1,11 +1,12 @@
-INSERT INTO `geckodb`.`geckos` (`name`, `age`, `origin`, `desc` , `father`, `mother`)
+INSERT INTO `geckodb`.`geckos` (`name`, `age`, `origin`, `desc` , `color`, `father`, `mother`, `thumbnail`, `images`, `ancestry`)
 VALUES
-('blue', 3, 'Korea', 'pretty blue gecko', null, null),
-('red', 3, 'India', 'pretty red gecko',null ,null),
-('yellow', 3, 'Brazil', 'pretty yellow gecko', null,null),
-('pink', 3, 'Japan', 'pretty pink gecko',null,null),
-('violet', 3, 'USA', 'pretty violet gecko',null,null),
-('skyblue', 3, 'Canada', 'pretty skyblue gecko',null,null);
+('Cutey', 3, 'Korea', 'pretty blue gecko', 'yellow', 1, 2, 'assets/images/yellow.jpeg', 'img1, img2', '2/'),
+('파랑이', 4, 'India', 'pretty red gecko','blue' ,1,2,'assets/images/blue.png', 'img1, img2', '1/3/'),
+('오렌지', 1, 'Brazil', 'pretty yellow gecko','orange', 1,2,'assets/images/orange.jpeg', 'img1, img2', '1/2/4/'),
+('레모니', 2, 'America', 'pretty pink gecko','lemon',1,2,'assets/images/lemon_frost_resized.png', 'img1, img2', '1/2/3/'),
+('모니', 2, 'USA', 'pretty violet gecko','lemon',1,2,'assets/images/lemon_frost_resized.png', 'img1, img2', '1/2/3/4/6/7/8/'),
+('utey', 2, 'Canada', 'pretty skyblue gecko','yellow',1,2,'assets/images/yellow.jpeg', 'img1, img2', '2/');
+('랑이', 4, 'Indonesia', 'pretty skyblue gecko','blue',1,2,'assets/images/blue.png', 'img1, img2', '1/3/');
 
 INSERT INTO `geckodb`.`users` (`name`, `age`, `desc` )
 VALUES
