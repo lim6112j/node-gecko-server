7334132e1a24e94365694e19b4ec89e2206b80bb
lim6112j <lim6112j@gmail.com>
2022-07-14 13:30:08 +0900

    privilege bug

--- scripts/create_user.sql
+++ scripts/create_user.sql
@@ -3,6 +3,6 @@ CREATE USER 'gecko'@'localhost' IDENTIFIED BY 'mySecretPassord';
 /* In an ideal world, we should strictly provide the grants the API has access
  * to a specific table rather than all the privileges.
  */
-GRANT ALL PRIVILEGES ON geckodb TO 'gecko'@'localhost';
+GRANT ALL PRIVILEGES ON geckodb.* TO 'gecko'@'localhost';
 
 flush privileges;
