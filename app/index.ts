1c049f16fa4a342a31c1a86f995dc78d6f3d38ae
lim6112j <lim6112j@gmail.com>
2022-07-15 15:05:53 +0900

    minio client added for testing

--- app/index.ts
+++ app/index.ts
@@ -9,9 +9,26 @@ import routes from './api/routes';
 //import logger from './api/middlewares/logger.middleware';
 //import errorHandler from './api/middlewares/error-handler.middleware';
 import * as MySQLConnector from './api/utils/mysql.connector';
+import Minio from 'minio';
 
 const app = express();
 const port = 3200;
+const minioClient = new Minio.Client({
+    endPoint: 'nstorage',
+    port: 9000,
+    accessKey: 'lim6112j',
+    secretKey: 'gogosin0'
+});
+var file = '/home/ubuntu/start.sh';
+var metaData = {
+    'Content-Type': 'application/octet-stream',
+    'X-amx-Meta-Testing': 1234,
+    'example': 5678
+};
+minioClient.fPutObject('images', 'startback.sh', file, metaData, function (err, etag) {
+    if (err) return console.log(err);
+    console.log("File uploaded successfully");
+});
 MySQLConnector.init();
 app.use(compression());
 app.use(helmet());
