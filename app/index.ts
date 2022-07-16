77b883dd500f0c7b1b3b501eebce6dff4ff4a7da
lim6112j <lim6112j@gmail.com>
2022-07-16 11:50:52 +0900

    env modified , api url match fake server

--- app/index.ts
+++ app/index.ts
@@ -36,7 +36,7 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/api/', routes);
+app.use('/', routes);
 app.listen(port, () => {
     console.log(`gecko server run on Port : ${port}`)
 });
